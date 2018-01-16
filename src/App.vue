74eb715775f5cb8d1b3fe42278cee97b9eb2ccac
KedAyAyA <caoxun333@gmail.com>
2018-01-16 11:51:13 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <audio-player></audio-player>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+// import AudioPlayer from '../dist/vueaudioplayer'
+import AudioPlayer from './components/AudioPlayer'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    AudioPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ src/components/AudioPlayer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="audio-player">
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .audio-player {
+    height: 80px;
+    border: 1px solid #eee;
+    box-sizing: border-box;
+    &__content {
+      color: yellow;
+    }
+  }
+</style>
